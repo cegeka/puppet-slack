86ba36c7458bc563e55e09493abd2ed038a60da6
Dieter De Meyer <dieterdemeyer@gmail.com>
2014-12-26 19:23:08 +0100

    Update example

--- examples/init.pp
+++ examples/init.pp
@@ -1,1 +1,8 @@
-include slack
+slack {
+  slack_webhook => 'INSERT_WEBHOOK',
+  slack_channel => '#puppet',
+  slack_username => 'Puppet',
+  slack_icon_url => 'https://pbs.twimg.com/profile_images/3672925108/954f7381089ac290b4690c5ffd9dd7d3.png',
+  slack_statuses => [ 'failed', 'changed' ],
+  foreman_api_host => 'INSERT_FOREMAN_URL'
+}
